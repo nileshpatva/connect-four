c82fd58209175dcfb27458c491fd9d1ac4787d69
Nilesh Patva <nileshpatva40@gmail.com>
2020-12-22 19:24:46 +0530

    basic board rendered with 6x7 grid: created 6 rows and 7 columns structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'connect-four';
+  // game board
+  board = [];
+  rows = 6;
+  columns = 7;
+
+  readonly EMPTY = 'empty';
+  readonly PLAYER_ONE = 'red';
+  readonly PLAYER_TWO = 'yellow';
+
+  constructor() {
+    for (let i = 0; i < this.rows; i++) {
+      this.board[i] = [];
+      for (let j = 0; j < this.columns; j++) {
+        this.board[i][j] = this.EMPTY;
+      }
+    }
+  }
+
+  ngOnInit() {
+    console.log(this.board);
+  }
 }
