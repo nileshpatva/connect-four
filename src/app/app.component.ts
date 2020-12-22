755f94452f69ecb95dac607c62292a51a4ffd917
Nilesh Patva <nileshpatva40@gmail.com>
2020-12-22 19:51:35 +0530

    added method to find available empty slot to fill switching current player after fill

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'connect-four';
+  readonly EMPTY = 'empty';
+  readonly PLAYER_ONE = 'red';
+  readonly PLAYER_TWO = 'yellow';
+
   // game board
   board = [];
   rows = 6;
   columns = 7;
 
-  readonly EMPTY = 'empty';
-  readonly PLAYER_ONE = 'red';
-  readonly PLAYER_TWO = 'yellow';
+  currentPlayer = '';
+
   /**
    * class constructor method,
    * used to initialize board with value "empty" for each slot
    */
   constructor() {
+    this.currentPlayer = this.PLAYER_ONE;
+
     for (let i = 0; i < this.rows; i++) {
       this.board[i] = [];
       for (let j = 0; j < this.columns; j++) {
@@ -34,7 +38,11 @@ export class AppComponent implements OnInit {
    */
   slotClicked(row, col) {
     console.log('slot: ', row, col);
-    this.fillSlot(row, col);
+
+    const available = this.getLastAvailableSlot(row, col);
+    console.log('available', available);
+
+    this.fillSlot(available, col);
   }
   /**
    * fill the slot with color
@@ -42,7 +50,26 @@ export class AppComponent implements OnInit {
    * @param col column index
    */
   private fillSlot(row, col) {
-    this.board[row][col] = this.PLAYER_ONE;
+    this.board[row][col] = this.currentPlayer;
+    this.currentPlayer =
+      this.currentPlayer === this.PLAYER_ONE
+        ? this.PLAYER_TWO
+        : this.PLAYER_ONE;
+  }
+
+  /**
+   * finds most bottom available slot for fill,
+   * if not available returns null
+   * @param row row index
+   * @param col column index
+   */
+  getLastAvailableSlot(row, col) {
+    for (let i = this.rows - 1; i >= 0; i--) {
+      if (this.board[i][col] === this.EMPTY) {
+        return i;
+      }
+    }
+    return null;
   }
 
   ngOnInit() {
