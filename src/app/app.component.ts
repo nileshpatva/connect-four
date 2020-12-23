ddf44be47c52b60f455c88b5bffd3c82d586fb7d
Nilesh Patva <nileshpatva40@gmail.com>
2020-12-23 10:01:04 +0530

    user message added to UI and horizontal plus vertical checks are added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,14 +16,15 @@ export class AppComponent implements OnInit {
   columns = 7;
 
   currentPlayer = '';
-
+  userMsg = '';
+  winningSlots = [];
   /**
    * class constructor method,
    * used to initialize board with value "empty" for each slot
    */
   constructor() {
     this.currentPlayer = this.PLAYER_ONE;
-
+    this.winningSlots = [];
     for (let i = 0; i < this.rows; i++) {
       this.board[i] = [];
       for (let j = 0; j < this.columns; j++) {
@@ -37,14 +38,25 @@ export class AppComponent implements OnInit {
    * @param col column index
    */
   slotClicked(row, col) {
-    console.log('slot: ', row, col);
+    // console.log('slot: ', row, col);
+    if (this.winningSlots.length > 0) return;
+    this.userMsg = '';
 
     const available = this.getLastAvailableSlot(row, col);
-    console.log('available', available);
+    // console.log('available', available);
     if (available === null) {
+      this.userMsg = 'No more slots available!';
       return;
     }
     this.fillSlot(available, col);
+    if (this.checkHorizontalStreak() || this.checkVerticalStreak()) {
+      this.userMsg = `Winner is ${this.currentPlayer.toUpperCase()}`;
+      return;
+    }
+    this.currentPlayer =
+      this.currentPlayer === this.PLAYER_ONE
+        ? this.PLAYER_TWO
+        : this.PLAYER_ONE;
   }
   /**
    * fill the slot with color
@@ -53,10 +65,6 @@ export class AppComponent implements OnInit {
    */
   private fillSlot(row, col) {
     this.board[row][col] = this.currentPlayer;
-    this.currentPlayer =
-      this.currentPlayer === this.PLAYER_ONE
-        ? this.PLAYER_TWO
-        : this.PLAYER_ONE;
   }
 
   /**
@@ -65,13 +73,87 @@ export class AppComponent implements OnInit {
    * @param row row index
    * @param col column index
    */
-  getLastAvailableSlot(row, col) {
+  private getLastAvailableSlot(row, col) {
     for (let i = this.rows - 1; i >= 0; i--) {
       if (this.board[i][col] === this.EMPTY) {
         return i;
       }
     }
     return null;
+  }
+  /**
+   * checks four values are same or not and checks if it is not empty
+   * @param first first slot value
+   * @param second second slot value
+   * @param third third slot value
+   * @param fourth fourth slot value
+   */
+  private checkAdjacentColors(first, second, third, fourth) {
+    return (
+      first !== this.EMPTY &&
+      first === second &&
+      first === third &&
+      first === fourth
+    );
+  }
+  /**
+   * Checks the horizontal four adjacent slots
+   * if they have same colors, non empty values then returns true
+   * else false
+   */
+  private checkHorizontalStreak() {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < 4; col++) {
+        if (
+          this.checkAdjacentColors(
+            this.board[row][col],
+            this.board[row][col + 1],
+            this.board[row][col + 2],
+            this.board[row][col + 3]
+          )
+        ) {
+          this.winningSlots = [
+            `${row},${col}`,
+            `${row},${col + 1}`,
+            `${row},${col + 2}`,
+            `${row},${col + 3}`,
+          ];
+          // console.log('winner', this.winningSlots);
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Checks the vertical four adjacent slots
+   * if they have same colors, non empty values then returns true
+   * else false
+   */
+  private checkVerticalStreak() {
+    for (let col = 0; col < this.columns; col++) {
+      for (let row = 0; row < 3; row++) {
+        if (
+          this.checkAdjacentColors(
+            this.board[row][col],
+            this.board[row + 1][col],
+            this.board[row + 2][col],
+            this.board[row + 3][col]
+          )
+        ) {
+          this.winningSlots = [
+            `${row},${col}`,
+            `${row + 1},${col}`,
+            `${row + 2},${col}`,
+            `${row + 3},${col}`,
+          ];
+          // console.log('winner', this.winningSlots);
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
   ngOnInit() {
